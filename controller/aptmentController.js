20a94bc70bf0e62481dd7553b5353c53b0fa6601
expog2001 <i.there.hariomsingh@gmail.com>
2023-11-20 10:48:57 +0530

    cha1

--- controller/aptmentController.js
+++ controller/aptmentController.js
@@ -35,7 +35,7 @@ const getAppointment = expressAsyncHandler(async (req, res) => {
     ) {
       return res.status(400).json({ message: "Invalid appointment time" });
     }
-    console.log(800);
+    console.log(8);
     const startTime = new Date(
       doctor.weeklySchedule[
         requestedTime.toLocaleDateString("en-US", { weekday: "long" })
