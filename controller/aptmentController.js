d0d88846e2a8419b260955b3fa318703f6480a72
expog2001 <i.there.hariomsingh@gmail.com>
2023-11-20 21:13:03 +0530

    jb

--- controller/aptmentController.js
+++ controller/aptmentController.js
@@ -23,7 +23,7 @@ const getAppointment = expressAsyncHandler(async (req, res) => {
     if (!req.body.date) {
       return res.status(400).json({ message: "Appointment date is required" });
     }
-    console.log(hello)
+    console.log(he)
     const receivedData = req.body;
     const requestedTime = new Date(receivedData.date);
     const requestedDay = requestedTime.getDay();
